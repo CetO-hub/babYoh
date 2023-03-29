da065bf846cd08d4918cfa977b19737ad83340f6
CetO-hub <102421594+CetO-hub@users.noreply.github.com>
2023-03-29 22:07:22 +0200

    feat: set up project and include logo

---
+++ src/Components/Navigation.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../assets/img/logo.jpg";
+
+const Navigation = () => {
+  return (
+    <>
+      <img src={logo} />
+    </>
+  );
+};
+
+export default Navigation;
