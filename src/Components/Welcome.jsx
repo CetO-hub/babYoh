720f142936e7842a3485c61dafc4741a75a79ac4
CetO-hub <102421594+CetO-hub@users.noreply.github.com>
2023-03-30 20:27:13 +0200

    feat: add mobile hero section design

--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -26,6 +26,21 @@ const Welcome = () => {
             src={isMobile ? babyHeroMobile : babyHero}
             data-testid="hero-image"
           />
+          <div className="hero__image-container" data-layer="true"></div>
+        </div>
+        <div className="hero__text-container">
+          <h1>
+            Welcome<span>Baby</span>
+          </h1>
+          <p>
+            Make notes, create images and track different stats about your baby
+          </p>
+          <div>
+            <a className="button" data-color="pink">
+              Signup
+            </a>
+            <a className="button">Login</a>
+          </div>
         </div>
       </div>
     </>
