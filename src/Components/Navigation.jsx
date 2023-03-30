359574f478ccdbcf4d6360cd0ff6d1be4faf6a0b
CetO-hub <102421594+CetO-hub@users.noreply.github.com>
2023-03-30 14:08:14 +0200

    feat: add navigation css

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -5,22 +5,33 @@ const Navigation = () => {
   return (
     <>
       <div className="row">
-        <div className="nav"></div>
-        <a href="/">
-          <img src={logo} data-testid="logo" />
-        </a>
-        <ul>
-          <li>
-            <a href="/login" data-testid="login">
-              Login
-            </a>
-          </li>
-          <li>
-            <a href="/signup" data-testid="signup">
-              Signup
-            </a>
-          </li>
-        </ul>
+        <div className="nav">
+          <a href="/" className="nav__logo">
+            <img src={logo} data-testid="logo" />
+          </a>
+          <ul className="nav__ul">
+            <li>
+              <a
+                href="/login"
+                className="button"
+                data-testid="login"
+                data-color="blue"
+              >
+                Login
+              </a>
+            </li>
+            <li>
+              <a
+                href="/signup"
+                className="button"
+                data-testid="signup"
+                data-color="pink"
+              >
+                Signup
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </>
   );
